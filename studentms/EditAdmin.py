a9248c03abcf2edc8e4f9cd2df0ec889ab629f2f
sky <sky@163.com>
2023-02-09 18:02:36 +0800

    修改代码

--- studentms/EditAdmin.py
+++ studentms/EditAdmin.py
@@ -10,9 +10,9 @@
 import sql_table
 
 from PySide6.QtWidgets import QApplication, QMainWindow, QMessageBox, QInputDialog, QLineEdit, QPushButton, QLabel
-from PySide6.QtCore import Qt, QRect
+from PySide6.QtCore import Qt, QRect, QMetaObject, QCoreApplication
 from ComboCheckBox import QComboCheckBox
-from db import DBSESSION
+from db import DBSESSION, md5
 
 
 class EditAdmin(object):
@@ -65,6 +65,80 @@ class EditAdmin(object):
         self.label_3.setGeometry(QRect(23, 140, 71, 20))
         self.label_3.setObjectName("label_3")
 
+        self.pushButton = QPushButton(Form)
+        self.pushButton.setGeometry(QRect(150, 220, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+
+        # 提交按钮
+        self.pushButton.clicked.connect(lambda: self.updateAdmin(adminId, Form))
+
+        self.pushButton_2 = QPushButton(Form)
+        self.pushButton_2.setGeometry(QRect(230, 220, 75, 23))
+        self.pushButton_2.setObjectName("pushButton_2")
+
+        # 取消按钮
+        self.pushButton_2.clicked.connect(lambda: Form.hide())
+
+        self.pushButton_3 = QPushButton(Form)
+        self.pushButton_3.setGeometry(QRect(10, 220, 75, 23))
+        self.pushButton_3.setObjectName("pushButton_3")
+
+        # 重置密码按钮
+        self.pushButton_3.clicked.connect(lambda: self.resetPassw(adminId, Form))
+
+        self.retranslateUi(Form)
+        QMetaObject.connectSlotsByName(Form)
+
+    # 重新翻译（针对性修改）
+    def retranslateUi(self, Form):
+        _translate = QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "编辑角色"))
+        self.label.setText(_translate("Form", "用户名："))
+        self.label_2.setText(_translate("Form", "备注："))
+        self.label_3.setText(_translate("Form", "可管理班级："))
+        self.pushButton.setText(_translate("Form", "提交"))
+        self.pushButton_2.setText(_translate("Form", "取消"))
+        self.pushButton_3.setText(_translate("Form", "重置密码"))
+
+    # 修改用户名
+    def updateAdmin(self, adminId, Form):
+        username = self.textEdit.text()
+        mark = self.textEdit_2.text()
+        classids = self.textEdit_3.get_class_text()
+        try:
+            admin = DBSESSION.query(sql_table.TAdmin).filter(sql_table.TAdmin.a_id == adminId).update(
+                {"a_username":f"{username}","a_mark":f"{mark}","a_classid":f"{classids}"})
+            DBSESSION.commit()
+            QMessageBox.about(Form, "成功", "编辑成功！请刷新数据列表。")
+        except Exception as e:
+            DBSESSION.rollback()
+            QMessageBox.about(Form, "失败", "编辑失败!")
+        Form.close()
+
+    # 重置密码
+    def resetPassw(self, adminId, Form):
+        text, okPressed = QInputDialog.getText(Form, "重置密码", "新密码:", QLineEdit.EchoMode.Normal, '')
+        if okPressed:
+            try:
+                DBSESSION.query(sql_table.TAdmin).filter(sql_table.TAdmin.a_id == adminId).update(
+                    {"a_password":f"{md5(text)}"})
+                DBSESSION.commit()
+                QMessageBox.about(Form, "成功", "重置密码成功！")
+            except Exception as  e:
+                DBSESSION.rollback()
+                QMessageBox.about(Form, "失败", "重置密码失败！")
+            Form.close()
+
+
+def main():
+    import sys
+    app = QApplication(sys.argv)
+    aw = EditAdmin()
+    window = QMainWindow()
+    aw.setup_ui(window, 1)
+    window.show()
+    window.setWindowTitle("学生管理系统-修改用户")
+    sys.exit(app.exec())
 
 if __name__ == "__main__":
-    pass
+    main()
